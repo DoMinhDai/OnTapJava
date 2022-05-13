20493f519d25e3e2c1ebbea0a4dd13ccca8a76f4
DoMinhDai <20a10010196@students.hou.edu.vn>
2022-05-13 10:09:19 +0700

    14

--- app/src/main/java/com/example/ontapjava/MainActivity.java
+++ app/src/main/java/com/example/ontapjava/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String t = " Day la";
+        String v = " v ";
     }
 }
